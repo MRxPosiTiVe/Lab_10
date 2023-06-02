d018744ad0ca681a07bef2714c045347f77a0194
Язов Михаил <100093644+MRxPosiTiVe@users.noreply.github.com>
2023-06-02 18:16:18 +0500

    Update exc10_1.java

--- exc10_1.java
+++ exc10_1.java
@@ -3,46 +3,24 @@ import java.io.*;
 
 public class exc10_1 {
 
-    public static void Write(char a, boolean bool) {
-
-        try (FileWriter writer = new FileWriter("src/exc_10_1.txt", bool)){
-            writer.append(a);
+    public static void writeToFile(String data, boolean append) {
+        try (FileWriter writer = new FileWriter("output.txt", append)) {
+            writer.write(data);
             writer.flush();
         } catch (IOException e) {
-            System.out.println("Error " + e);
+            System.out.println("Error: " + e.getMessage());
         }
-//        try (FileWriter writer = new FileWriter("src/exc_10_1.txt", bool)) {
-//            writer.append(a);
-//            writer.flush();
-//        } catch (IOException ex) {
-//
-//            System.out.println(ex.getMessage());
-//        }
     }
 
-    public static List<String> Read(String filename) {
+    public static List<String> readFromFile(String filename) {
         List<String> lines = new ArrayList<>();
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(filename));
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
-
             while ((line = br.readLine()) != null) {
-                char[] characters = line.toCharArray();
-                for (char c : characters) {
-                    lines.add(String.valueOf(c));
-                }
+                lines.add(line);
             }
         } catch (IOException e) {
-            System.out.println("Error " + e);
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-            } catch (IOException e) {
-                System.out.println("Error " + e);
-            }
+            System.out.println("Error: " + e.getMessage());
         }
         return lines;
     }
@@ -50,103 +28,116 @@ public class exc10_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Введите что-нибудь: ");
+        System.out.print("Введите символ для анализа: ");
         String symbol = input.nextLine();
 
-        System.out.println("Длина массива: ");
+        System.out.print("Длина массива: ");
         int len = input.nextInt();
 
         String alphabet = "ё1234567890-=йцукенгшщзхъфывапролджэячсмитьбю.!№;%:?*()_+, /[]{}@#$^&~ЁЙЦУ" +
                 "КЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
         Random random = new Random();
 
-        Write(' ', false);
+        writeToFile(" ", false);
         for (int i = 0; i < len; i++) {
             char c = alphabet.charAt(random.nextInt(alphabet.length()));
-            Write(c, true);
+            writeToFile(String.valueOf(c), true);
         }
 
-        String[] arr = new String[len + 1];
-        List<String> fileLines = Read("src/exc_10_1.txt");
-        System.out.println(fileLines);
+        List<String> fileLines = readFromFile("output.txt");
 
-        for (int i = 0; i < fileLines.size(); i++){
-            arr[i] = "";
-            arr[i] += fileLines.get(i);
-        }
-
-
-        System.out.println(Arrays.toString(arr));
+        StringBuilder output = new StringBuilder();
 
         int counter = 0;
-
-        for (String s : arr) {
-            boolean letters = s.matches("[а-яa-z]");
-            if (letters) counter++;
+        for (String line : fileLines) {
+            char[] characters = line.toCharArray();
+            for (char c : characters) {
+                if (Character.isLowerCase(c) && Character.UnicodeBlock.of(c) == Character.UnicodeBlock.CYRILLIC) {
+                    counter++;
+                }
+            }
         }
-        System.out.printf("Кол-во строчных букв %d\n", counter);
+        output.append("\nКоличество строчных русских букв: ").append(counter).append("\n");
 
-        int sym_count = 0;
-
-        for (String s : arr) {
-            if (s.equals(symbol)) sym_count++;
+        int symbolCount = 0;
+        for (String line : fileLines) {
+            char[] characters = line.toCharArray();
+            for (char c : characters) {
+                if (String.valueOf(c).equals(symbol)) {
+                    symbolCount++;
+                }
+            }
         }
-        System.out.printf("Символов введеных с клавиатуры: %d\n", sym_count);
+        output.append("Количество символов '").append(symbol).append("' в массиве: ").append(symbolCount).append("\n");
 
         if (symbol.matches("[0-9]+")) {
             int digit = Integer.parseInt(symbol);
             int digitCountInArray = 0;
-            for (String s : arr) {
-                if (s.matches("\\d") && Integer.parseInt(s) == digit) {
-                    digitCountInArray++;
+            for (String line : fileLines) {
+                char[] characters = line.toCharArray();
+                for (char c : characters) {
+                    if (Character.isDigit(c) && Integer.parseInt(String.valueOf(c)) == digit) {
+                        digitCountInArray++;
+                    }
                 }
             }
             if (digitCountInArray >= 2) {
-                System.out.println("Среди символов есть 2 и более цифры, входящие в цифру " + digit);
+                output.append("Среди символов есть 2 и более цифры, входящие в число ").append(digit).append("\n");
             }
         } else {
-            System.out.println("Херня введенная с клавиатруры не число!");
+            output.append("Введенный символ не является числом!\n");
         }
 
-        int cout_brackets = 0;
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i].matches("\\(\\[\\{") && arr[i + 1].matches("\\)\\]\\}")) cout_brackets++;
+        int bracketPairsCount = 0;
+        for (int i = 0; i < fileLines.size() - 1; i++) {
+            String current = fileLines.get(i);
+            String next = fileLines.get(i + 1);
+            if ((current.equals("(") && next.equals(")")) ||
+                    (current.equals("[") && next.equals("]")) ||
+                    (current.equals("{") && next.equals("}"))) {
+                bracketPairsCount++;
+            }
         }
-        System.out.printf("Кол-во соседних закрытых скобок: %d\n", cout_brackets);
+        if (bracketPairsCount > 0) {
+            output.append("Количество соседних закрытых скобок: ").append(bracketPairsCount).append("\n");
+        }
 
-        int space_counter = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(" ")) space_counter++;
-            else space_counter = 0;
-            if (space_counter > 2) {
-                System.out.println("Обнаружены более двух подряд идущих пробелов.");
+        int spaceCounter = 0;
+        for (String line : fileLines) {
+            if (line.equals(" ")) {
+                spaceCounter++;
+            } else {
+                spaceCounter = 0;
+            }
+            if (spaceCounter > 2) {
+                output.append("Обнаружено более двух подряд идущих пробелов.\n");
                 break;
             }
         }
 
         boolean foundSequence = false;
-        for (int i = 0; i < arr.length - 3; i++) {
-            if (Character.isDigit(arr[i].charAt(0)) &&
-                    Character.isDigit(arr[i + 1].charAt(0)) &&
-                    Character.isDigit(arr[i + 2].charAt(0))) {
-                int num1 = Integer.parseInt(arr[i]);
-                int num2 = Integer.parseInt(arr[i + 1]);
-                int num3 = Integer.parseInt(arr[i + 2]);
-                if (num1 > num2 && num2 > num3) {
+        for (int i = 0; i < fileLines.size() - 2; i++) {
+            String num1 = fileLines.get(i);
+            String num2 = fileLines.get(i + 1);
+            String num3 = fileLines.get(i + 2);
+            if (num1.matches("\\d") && num2.matches("\\d") && num3.matches("\\d")) {
+                int n1 = Integer.parseInt(num1);
+                int n2 = Integer.parseInt(num2);
+                int n3 = Integer.parseInt(num3);
+                if (n1 > n2 && n2 > n3) {
                     foundSequence = true;
                     break;
                 }
             }
         }
         if (foundSequence) {
-            System.out.println("Существуют такие натуральные i и j, что i<k<j<п и " +
-                    "si, и si+1 убывающая последовательность цифр, a sj и sj+1 возрастающая последовательность цифр.");
+            output.append("Существуют такие натуральные i и j, что i < k < j < п и si, и si+1 убывающая последовательность цифр, а sj и sj+1 возрастающая последовательность цифр.\n");
         } else {
-            System.out.println("Не найдено таких натуральных i и j, что i<k<j<п и " +
-                    "si, и si+1 убывающая последовательность цифр, a sj и sj+1 возрастающая последовательность цифр.");
+            output.append("Не найдено таких натуральных i и j, что i < k < j < п и si, и si+1 убывающая последовательность цифр, а sj и sj+1 возрастающая последовательность цифр.\n");
         }
 
+        writeToFile(output.toString(), true);
+        System.out.println("Результаты записаны в файл 'output.txt'.");
     }
 }
+
